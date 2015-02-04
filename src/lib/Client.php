52c99cc07cab5e695e57c9a42d76df8964b1c056
Paul Waring <paul.waring@manchester.ac.uk>
2015-02-04 16:21:47 +0000

    Function for fetching a given API endpoint

--- src/lib/Client.php
+++ src/lib/Client.php
@@ -4,5 +4,15 @@ namespace UoMCS\OpenBadges\Frontend;
 
 class Client
 {
-  
+  public static function get($path)
+  {
+    $url = OPEN_BADGES_BACKEND_URL . $path;
+
+    $client = new \Zend\Http\Client();
+
+    $request = new \Zend\Http\Request();
+    $request->setUri($url);
+
+    return $client->send($request);
+  }
 }
