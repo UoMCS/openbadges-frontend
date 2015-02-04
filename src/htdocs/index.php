41de4667e83a13ab30c1d1006ab87fc504283e14
Paul Waring <paul.waring@manchester.ac.uk>
2015-02-04 13:55:31 +0000

    Skeleton index file

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,1 +1,9 @@
 <?php
+
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = OPEN_BADGES_DEBUG_MODE;
+$app->run();
