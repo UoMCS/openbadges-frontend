fe16bc8cb63f56b5c9959a3d1c09a454b3deb873
Paul Waring <paul.waring@manchester.ac.uk>
2015-02-05 10:15:57 +0000

    Badges URL

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -4,7 +4,25 @@ require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../autoload.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use UoMCS\OpenBadges\Frontend\Client;
+
 $app = new Silex\Application();
+
+$app->get('/badges', function() use ($app) {
+  $response = Client::get('/badges');
+
+  if (!$response->isOk())
+  {
+    $app->abort($response->getStatusCode(), 'Error retrieving data: ' . $response->getBody());
+  }
+
+  $loader = new Twig_Loader_Filesystem(TWIG_TEMPLATES_DIR);
+  $twig = new Twig_Environment($loader);
+
+  $badges = json_decode($response->getBody(), true);
+
+  return $twig->render('badges/index.html', array('badges' => $badges));
+});
 
 $app['debug'] = OPEN_BADGES_DEBUG_MODE;
 $app->run();
