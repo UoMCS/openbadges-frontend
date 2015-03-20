ed68643c33889a403eacc6734d328151f25ce614
Paul Waring <paul.waring@manchester.ac.uk>
2015-03-20 11:18:35 +0000

    Switch to composer autoloader

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,8 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../autoload.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use UoMCS\OpenBadges\Frontend\Client;
 
